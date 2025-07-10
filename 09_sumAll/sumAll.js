68654bc45f13b38c826fe61c79addc96e431ff52
Eduard Bohn <eduard.bohn@mail.de>
2025-07-10 19:37:32 +0200

    09 complete

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+let buffer;
+let result = 0;
+if (
+    start < 0 || end < 0 ||
+    start % 1 != 0 || end % 1 != 0 ||
+    typeof(start) != "number" || typeof(end) != "number"
+) {
+    return "ERROR";
+}
+if(start > end) {
+    buffer = start;
+    start = end; 
+    end = buffer;
+}
+for (let i = start; i <= end; i++) {
+    result += i;
+}
+return result;
 };
 
 // Do not edit below this line
